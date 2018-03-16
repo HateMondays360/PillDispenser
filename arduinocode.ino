8f6e1276a056bcb677d97a35325f270efec4043d
Christian Kuss <christianmkuss@gmail.com>
2018-03-16 02:19:35 -0400

    Update arduinocode.ino

--- arduinocode.ino
+++ arduinocode.ino
@@ -1,11 +1,11 @@
 #include <Servo.h>
 
 Servo Motor;
-
 int n;
 
 void setup(){
   Serial.begin(9600);
+  pinMode(8, OUTPUT);
   Motor.attach(13);
   Motor.write(90);
   n = 0;
@@ -13,10 +13,13 @@ void setup(){
 
 void loop(){
   Motor.write(90);
+  digitalWrite(8, LOW);
   if(Serial.available())
     n = Serial.read() - '0';
   if(n==1){
     Motor.write(0);
-    delay(5000);
+    digitalWrite(8, HIGH);
+    delay(3000);
+    n = 0;
   }
 }
