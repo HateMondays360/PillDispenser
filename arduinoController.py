6baad4c7fa13df9d608fcbf4ebbcd067b5410746
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-03-23 17:37:38 -0400

    better mp3 playing

--- arduinoController.py
+++ arduinoController.py
@@ -10,6 +10,7 @@ URL = "0.0.0.0:80"
 
 arduinoSerialData.write('1')
 while True:
+    mp3 = False
     Pill1_Hour_List = []
     Pill1_Minute_List = []
     Pill2_Hour_List = []
@@ -39,12 +40,10 @@ while True:
         Pill2_Hour_List.append(this_time[0])
         Pill2_Minute_List.append(this_time[2])
 
-
     for element in pill3:
         this_time = element.text.split()
         Pill3_Hour_List.append(this_time[0])
         Pill3_Minute_List.append(this_time[2])
-
 
     # Update the current time
     now = datetime.datetime.now()
@@ -53,17 +52,19 @@ while True:
         if now.hour == int(Pill1_Hour_List[x]) and now.minute == int(Pill1_Minute_List[x]):
             arduinoSerialData.write('1')
             print("Dispensing Pill")
-	    os.system('omxplayer -o local pillsdispensed.mp3')
+            mp3 = True
     for x in range(len(Pill2_Hour_List)):
         if now.hour == int(Pill2_Hour_List[x]) and now.minute == int(Pill2_Minute_List[x]):
             arduinoSerialData.write('2')
             print("Dispensing Pill")
-	    os.system('omxplayer -o local pillsdispensed.mp3')
+            mp3 = True
     for x in range(len(Pill3_Hour_List)):
         if now.hour == int(Pill3_Hour_List[x]) and now.minute == int(Pill3_Minute_List[x]):
             arduinoSerialData.write('3')
             print("Dispensing Pill")
-	    os.system('omxplayer -o local pillsdispensed.mp3')
+            mp3 = True
+    if mp3:
+        os.system('omxplayer -o local pillsdispensed.mp3')
     time.sleep(60)
 
 
