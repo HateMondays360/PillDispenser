e787cf3b76fa7731ef98a96d4a15aca999663585
Christian Kuss <christianmkuss@gmail.com>
2018-03-23 16:01:25 -0400

    audion files

--- AlexaSkill.py
+++ AlexaSkill.py
@@ -57,5 +57,4 @@ def gpio_control():
 
 
 if __name__ == '__main__':
-    port = 5000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
--- arduinoController.py
+++ arduinoController.py
@@ -3,6 +3,7 @@ import requests
 import serial
 import datetime
 import time
+import os
 
 arduinoSerialData = serial.Serial('/dev/ttyUSB0', 9600)
 URL = "0.0.0.0:80"
@@ -16,7 +17,7 @@ while True:
     Pill3_Hour_List = []
     Pill3_Minute_List = []
     # Get the website data from the URL
-    r = requests.get("http://10.60.157.218")
+    r = requests.get("http://0.0.0.0:80")
     
     # Grab and transcribe the raw html using Beautiful Soup
     data = r.content
@@ -52,14 +53,17 @@ while True:
         if now.hour == int(Pill1_Hour_List[x]) and now.minute == int(Pill1_Minute_List[x]):
             arduinoSerialData.write('1')
             print("Dispensing Pill")
+	    os.system('omxplayer -o local pillsdispensed.mp3')
     for x in range(len(Pill2_Hour_List)):
         if now.hour == int(Pill2_Hour_List[x]) and now.minute == int(Pill2_Minute_List[x]):
             arduinoSerialData.write('2')
             print("Dispensing Pill")
+	    os.system('omxplayer -o local pillsdispensed.mp3')
     for x in range(len(Pill3_Hour_List)):
         if now.hour == int(Pill3_Hour_List[x]) and now.minute == int(Pill3_Minute_List[x]):
             arduinoSerialData.write('3')
             print("Dispensing Pill")
+	    os.system('omxplayer -o local pillsdispensed.mp3')
     time.sleep(60)
 
 
