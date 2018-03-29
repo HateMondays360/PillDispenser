0d079b081a498d7c5519928b614e707538fc3ce0
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-03-29 14:28:38 -0400

    made for three pills

--- AlexaSkill.py
+++ AlexaSkill.py
@@ -8,7 +8,26 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+first = True
+
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+def string_from_lists(HourList, MinuteList, am_or_pm, pill_name):
+    global first
+    pills = ''
+    for x in range(len(HourList)):
+        if HourList[x] > 12:
+            HourList[x] %= 12
+            am_or_pm.append(' PM ')
+        else:
+            am_or_pm.append(' AM ')
+        if first:
+            pills = str(HourList[x]) + ' ' + str(MinuteList[x]) + am_or_pm[x]
+            first = False
+        else:
+            pills += 'and' + str(HourList[x]) + ' ' + str(MinuteList[x]) + am_or_pm[x]
+    return pill_name + " at " + pills
 
 
 @ask.intent('GPIOControlIntent', mapping={})
@@ -20,7 +39,6 @@ def gpio_control():
     Pill2_Minute_List = []
     Pill3_Hour_List = []
     Pill3_Minute_List = []
-    pill1name = ''
     am_or_pm = []
 
     try:
@@ -32,6 +50,8 @@ def gpio_control():
         pill2 = page_soup.findAll("td", {"class": "pill2Time"})
         pill3 = page_soup.findAll("td", {"class": "pill3Time"})
         pill1name = page_soup.find("p", {"id": "pill1name"}).text
+        pill2name = page_soup.fine("p", {"id": "pill2name"}).text
+        pill3name = page_soup.fine("p", {"id": "pill3name"}).text
 
         # Loop through all elements in the list of pill times and store them in lists
         for element in pill1:
@@ -51,19 +71,13 @@ def gpio_control():
 
     except Exception as e:
         return statement('You have no pills.')
-    first = True
-    for x in range(len(Pill1_Hour_List)):
-        if Pill1_Hour_List[x] > 12:
-            Pill1_Hour_List[x] %= 12
-            am_or_pm.append(' PM ')
-        else:
-            am_or_pm.append(' AM ')
-        if first:
-            pills = str(Pill1_Hour_List[x]) + ' ' + str(Pill1_Minute_List[x]) + am_or_pm[x]
-            first = False
-        else:
-            pills += 'and' + str(Pill1_Hour_List[x]) + ' ' + str(Pill1_Minute_List[x]) + am_or_pm[x]
-    return statement('You have {} at {}'.format(pill1name, pills))
+    if len(Pill1_Hour_List) > 0:
+        pills += string_from_lists(Pill1_Hour_List, Pill1_Minute_List, am_or_pm, pill1name)
+    if len(Pill2_Hour_List) > 0:
+        pills += string_from_lists(Pill2_Hour_List, Pill2_Minute_List, am_or_pm, pill2name)
+    if len(Pill3_Hour_List) > 0:
+        pills += string_from_lists(Pill3_Hour_List, Pill3_Minute_List, am_or_pm, pill3name)
+    return statement('You have {}'.format(pills))
 
 
 if __name__ == '__main__':
