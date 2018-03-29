cd8ceadcdb9342f65be82b09a06c27af6155d340
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-03-29 14:41:06 -0400

    made for three pills

--- AlexaSkill.py
+++ AlexaSkill.py
@@ -4,7 +4,6 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
-
 app = Flask(__name__)
 ask = Ask(app, '/')
 
@@ -41,35 +40,33 @@ def gpio_control():
     Pill3_Minute_List = []
     am_or_pm = []
 
-    try:
-        r = requests.get("http://0.0.0.0:80")
-        data = r.content
-        page_soup = BeautifulSoup(data, "html.parser")
+    r = requests.get("http://0.0.0.0:80")
+    data = r.content
+    page_soup = BeautifulSoup(data, "html.parser")
 
-        pill1 = page_soup.findAll("td", {"class": "pill1Time"})
-        pill2 = page_soup.findAll("td", {"class": "pill2Time"})
-        pill3 = page_soup.findAll("td", {"class": "pill3Time"})
-        pill1name = page_soup.find("p", {"id": "pill1name"}).text
-        pill2name = page_soup.fine("p", {"id": "pill2name"}).text
-        pill3name = page_soup.fine("p", {"id": "pill3name"}).text
+    pill1 = page_soup.findAll("td", {"class": "pill1Time"})
+    pill2 = page_soup.findAll("td", {"class": "pill2Time"})
+    pill3 = page_soup.findAll("td", {"class": "pill3Time"})
+    pill1name = page_soup.find("p", {"id": "pill1name"}).text
+    pill2name = page_soup.fine("p", {"id": "pill2name"}).text
+    pill3name = page_soup.fine("p", {"id": "pill3name"}).text
 
-        # Loop through all elements in the list of pill times and store them in lists
-        for element in pill1:
-            this_time = element.text.split()
-            Pill1_Hour_List.append(int(this_time[0]))
-            Pill1_Minute_List.append(int(this_time[2]))
+    # Loop through all elements in the list of pill times and store them in lists
+    for element in pill1:
+        this_time = element.text.split()
+        Pill1_Hour_List.append(int(this_time[0]))
+        Pill1_Minute_List.append(int(this_time[2]))
 
-        for element in pill2:
-            this_time = element.text.split()
-            Pill2_Hour_List.append(int(this_time[0]))
-            Pill2_Minute_List.append(int(this_time[2]))
+    for element in pill2:
+        this_time = element.text.split()
+        Pill2_Hour_List.append(int(this_time[0]))
+        Pill2_Minute_List.append(int(this_time[2]))
 
-        for element in pill3:
-            this_time = element.text.split()
-            Pill3_Hour_List.append(int(this_time[0]))
-            Pill3_Minute_List.append(int(this_time[2]))
-    # except Exception as e:
-    #     return statement('You have no pills.')
+    for element in pill3:
+        this_time = element.text.split()
+        Pill3_Hour_List.append(int(this_time[0]))
+        Pill3_Minute_List.append(int(this_time[2]))
+
     if len(Pill1_Hour_List) > 0:
         pills += string_from_lists(Pill1_Hour_List, Pill1_Minute_List, am_or_pm, pill1name)
     if len(Pill2_Hour_List) > 0:
