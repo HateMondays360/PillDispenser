31d7aab65bc0d83c5748f21961bcc54d2a1ddd93
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-04-03 13:17:29 -0400

    changed things

--- clientview.py
+++ clientview.py
@@ -18,42 +18,42 @@ Pill3_Name = ''
 Pill3_Num = []
 
 URL = "0.0.0.0:80"  # Get the website data from the URL
-r = requests.get("http://0.0.0.0:80")
-
-# Grab and transcribe the raw html using Beautiful Soup
-data = r.content
-page_soup = BeautifulSoup(data, "html.parser")
-
-# Find and store all the time elements on the webpage
-pill1 = page_soup.findAll("td", {"class": "pill1Time"})
-pill2 = page_soup.findAll("td", {"class": "pill2Time"})
-pill3 = page_soup.findAll("td", {"class": "pill3Time"})
-numpill1 = page_soup.findAll("td", {"class": "pill1num"})
-numpill2 = page_soup.findAll("td", {"class": "pill2num"})
-numpill3 = page_soup.findAll("td", {"class": "pill3num"})
-
-# Loop through all elements in the list of pill times and store them in lists
-for x in range(len(pill1)):
-    Pill1_Num.append(int(numpill1[x].text))
-    this_time = pill1[x].text.split()
-    Pill1_Hour_List.append(this_time[0])
-    Pill1_Minute_List.append(this_time[2])
-
-for x in range(len(pill2)):
-    Pill2_Num.append(int(numpill2[x].text))
-    this_time = pill2[x].text.split()
-    Pill2_Hour_List.append(this_time[0])
-    Pill2_Minute_List.append(this_time[2])
-
-for x in range(len(pill3)):
-    Pill3_Num.append(int(numpill3[x].text))
-    this_time = pill3[x].text.split()
-    Pill3_Hour_List.append(this_time[0])
-    Pill3_Minute_List.append(this_time[2])
-
 
 @app.route("/")
 def main():
+    r = requests.get("http://0.0.0.0:80")
+
+    # Grab and transcribe the raw html using Beautiful Soup
+    data = r.content
+    page_soup = BeautifulSoup(data, "html.parser")
+
+    # Find and store all the time elements on the webpage
+    pill1 = page_soup.findAll("td", {"class": "pill1Time"})
+    pill2 = page_soup.findAll("td", {"class": "pill2Time"})
+    pill3 = page_soup.findAll("td", {"class": "pill3Time"})
+    numpill1 = page_soup.findAll("td", {"class": "pill1num"})
+    numpill2 = page_soup.findAll("td", {"class": "pill2num"})
+    numpill3 = page_soup.findAll("td", {"class": "pill3num"})
+
+    # Loop through all elements in the list of pill times and store them in lists
+    for x in range(len(pill1)):
+        Pill1_Num.append(int(numpill1[x].text))
+        this_time = pill1[x].text.split()
+        Pill1_Hour_List.append(this_time[0])
+        Pill1_Minute_List.append(this_time[2])
+
+    for x in range(len(pill2)):
+        Pill2_Num.append(int(numpill2[x].text))
+        this_time = pill2[x].text.split()
+        Pill2_Hour_List.append(this_time[0])
+        Pill2_Minute_List.append(this_time[2])
+
+    for x in range(len(pill3)):
+        Pill3_Num.append(int(numpill3[x].text))
+        this_time = pill3[x].text.split()
+        Pill3_Hour_List.append(this_time[0])
+        Pill3_Minute_List.append(this_time[2])
+
     templateData = {
         'Pill1_Hour_List': Pill1_Hour_List,
         'Pill1_Minute_List': Pill1_Minute_List,
