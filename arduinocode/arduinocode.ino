350efce5726b2cd56e0f2026789dae1ea234acd0
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-04-06 00:11:31 -0400

    added water and lights

--- arduinocode/arduinocode.ino
+++ arduinocode/arduinocode.ino
@@ -125,6 +125,13 @@ void rainbowCycle(uint8_t wait) {
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      if(Serial.available()){  //if the serial monitor is open then read it
+        String value = "";
+        value = Serial.readString();
+        if(value.length()>0){
+         break;
+        }
+      }
     }
     strip.show();
     delay(wait);
